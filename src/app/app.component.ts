2219327e2aafdb95764405e84a718e0e80fbfdb7
DawidWebb <tomasz.matras@developerweb.pl>
2023-02-02 23:04:05 +0100

    Add bootstap, mode.ts, del. sandbox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,60 @@
 import { Component } from '@angular/core';
+import { Customer } from './model';
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    <router-outlet></router-outlet>
-  `,
-  styles: []
+	selector: 'app-root',
+	template: `
+		<div class="container" style="margin-top: 2rem">
+			<div class="card" style="padding: 1rem">
+				<div class="card-title">
+					<h1 [style.color]="nameColor" [class.isActive]="isActive">{{ customer.name }}</h1>
+				</div>
+				<div class="card-body">
+					<div class="row">
+						<div class="col-sm-8">
+							<p>{{ customer.description }}</p>
+							<p>Wiek: {{ customer.age }}</p>
+							<p>Adres: {{ customer.adress.street }} {{ customer.adress.houseNumber }}, {{ customer.adress.city }}</p>
+							<button class="btn btn-outline-primary" (click)="changeIsActive()" type="button">Przełacz podkreślenie</button>
+							<button class="btn btn-outline-primary" (click)="onChangeColor()" type="button">Przełącz kolor</button>
+						</div>
+						<div class="col-sm-4">
+							<img [src]="customer.photoUrl" alt="" class="img-thumbnail"/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	`,
+	styles: [
+		`
+			.isActive {
+				text-decoration: underline;
+			}
+		`,
+	],
 })
 export class AppComponent {
-  title = 'customer-manager';
+	customer: Customer = {
+		name: 'Jan Kowalski',
+		photoUrl: 'assets/images/cat.jpg',
+		description: 'important client',
+		age: 45,
+		adress: {
+			street: 'Pomarańczowa',
+			houseNumber: 124,
+			city: 'Tłuszcz',
+		},
+	};
+
+	nameColor: string = 'blue';
+	isActive: boolean = true;
+	constructor() {}
+
+	changeIsActive() {
+		this.isActive = !this.isActive;
+	}
+
+	onChangeColor(){}
 }
+
